83f4600769996d8123067087e88ed7a0e12a8905
dlik <sber-9191@yandex.ru>
2017-09-13 14:24:40 +0500

    Обработал нажатие кнопки проигрывать

--- AudioPlayer/app/src/main/java/com/example/home/audioplayer/MainActivity.java
+++ AudioPlayer/app/src/main/java/com/example/home/audioplayer/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.home.audioplayer;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView tView;
     private int state;
 
+    private final int STATE_STOPED = 0;
     private final int STATE_PLAYED = 1;
     private final int STATE_PAUSED = 2;
 
@@ -25,5 +28,41 @@ public class MainActivity extends AppCompatActivity {
         bPlayPause = (ImageButton) findViewById(R.id.bPlayPause);
         bDestroi= (ImageButton)findViewById(R.id.bDestroi);
         tView = (TextView) findViewById(R.id.tView);
+        state = STATE_STOPED;
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bDestroi:
+                Toast.makeText(this, "Мы над этим ещё работаем", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bPlayPause:
+                preperePlayerState();
+                break;
+            default:
+                Toast.makeText(this, "Мы над жтим ещё работаем", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void preperePlayerState() {
+        switch (state){
+            case STATE_STOPED:
+                mediaPlayer.stop();
+
+                break;
+            case STATE_PAUSED:
+                mediaPlayer.pause();
+                bPlayPause.setImageResource(android.R.drawable.ic_media_pause);
+                tView.setText("Продолжим?");
+                state = STATE_PLAYED;
+
+                break;
+            case STATE_PLAYED:
+                mediaPlayer.pause();
+                bPlayPause.setImageResource(android.R.drawable.ic_media_play);
+                state = STATE_PAUSED;
+                break;
+        }
     }
 }
