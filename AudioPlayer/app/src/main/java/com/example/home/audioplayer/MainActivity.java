6695268ff9092806f5bf467e981e57c533c40030
dlik <sber-9191@yandex.ru>
2017-09-13 15:13:36 +0500

    копировал код из рабочей версии т.к. сравнил все 1 в 1 но у меня работает не так как надо

--- AudioPlayer/app/src/main/java/com/example/home/audioplayer/MainActivity.java
+++ AudioPlayer/app/src/main/java/com/example/home/audioplayer/MainActivity.java
@@ -10,68 +10,62 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton bDestroi, bPlayPause,bStop;
-    private MediaPlayer mediaPlayer;
-    private TextView tView;
-    private int state;
-
     private final int STATE_STOPED = 0;
     private final int STATE_PLAYED = 1;
     private final int STATE_PAUSED = 2;
 
+    private int state;
+    private ImageButton bPlayPause, bStop, idDestroi;
+    private MediaPlayer mediaPlayer;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bPlayPause = (ImageButton) findViewById(R.id.bPlayPause);
+        bPlayPause= (ImageButton) findViewById(R.id.bPlayPause);
         bStop = (ImageButton)findViewById(R.id.bStop);
-        bDestroi= (ImageButton)findViewById(R.id.bDestroi);
-        bDestroi.setBackgroundResource(R.drawable.layerdrawable);
-        tView = (TextView) findViewById(R.id.tView);
-        state = STATE_STOPED;
+        idDestroi = (ImageButton) findViewById(R.id.bDestroi);
+        idDestroi.setBackgroundResource(R.drawable.layerdrawable);
+        text = (TextView) findViewById(R.id.text);
         mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.muzmoru);
-
-        bPlayPause.setOnClickListener(this);
+        state = STATE_STOPED;
 
     }
 
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bDestroi:
-                Toast.makeText(this, "Мы над этим ещё работаем", Toast.LENGTH_SHORT).show();
-                break;
+    public void onClick(View view) {
+        switch (view.getId()){
             case R.id.bPlayPause:
                 preparePlayerState();
                 break;
             case R.id.bStop:
                 mediaPlayer.stop();
                 bPlayPause.setImageResource(android.R.drawable.ic_media_play);
-                tView.setText("Стоп");
+                text.setText("Stop");
                 state = STATE_STOPED;
-            default:
-                Toast.makeText(this, "Мы над жтим ещё работаем", Toast.LENGTH_SHORT).show();
+                break;
         }
     }
-
     private void preparePlayerState() {
         switch (state){
-            case STATE_STOPED:
+            case STATE_STOPED :
                 mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.muzmoru);
                 mediaPlayer.start();
                 bPlayPause.setImageResource(android.R.drawable.ic_media_pause);
+                text.setText("Play");
+                state = STATE_PLAYED;
                 break;
             case STATE_PLAYED:
                 mediaPlayer.pause();
                 bPlayPause.setImageResource(android.R.drawable.ic_media_play);
-                tView.setText("Pause");
+                text.setText("Pause");
                 state = STATE_PAUSED;
                 break;
             case STATE_PAUSED:
                 mediaPlayer.start();
                 bPlayPause.setImageResource(android.R.drawable.ic_media_pause);
-                tView.setText("Continue");
+                text.setText("Continue");
                 state = STATE_PLAYED;
                 break;
         }
