11de8dbd4047c6df30861423e32d30786ab7fe8c
dlik <sber-9191@yandex.ru>
2017-10-03 17:43:03 +0500

    Сделал отображение файлов из папки /sdcard/Music

--- AudioPlayer/app/src/main/java/com/example/home/audioplayer/SecondActivity.java
+++ AudioPlayer/app/src/main/java/com/example/home/audioplayer/SecondActivity.java
@@ -9,24 +9,66 @@ import android.widget.ListView;
  * Created by home on 25.09.2017.
  */
 
-public class SecondActivity extends Activity {
-    private static final String[] namber = {"1","2" ,"3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"};
-    private ArrayAdapter<String>daNamber;
-    private ListView listView;
+public class SecondActivity extends ListActivity {
+
+    private List<String> directoryEntries = new ArrayList<String>();
+    private File currentDirectory = new File("/");
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-
-        listView = (ListView) findViewById(R.id.listView);
-
-        daNamber = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, namber);
-        listView.setAdapter(daNamber);
-        //setListAdapter(new ArrayAdapter<String>());
-
-
-        this.setTitle("SecondActivity");
+        //перейдите в корневой каталог
+        browseTo(new File("/sdcard/Music"));
     }
 
+    //перейдите в родительский каталог
+    private void upOneLevel(){
+        if(this.currentDirectory.getParent() != null) {
+            this.browseTo(this.currentDirectory.getParentFile());
+        }
+    }
 
+    //просматривать файл или каталог
+    private void browseTo(final File aDirectory){
+        //если мы хотим просмотреть каталог
+        if (aDirectory.isDirectory()){
+            //заполнить список файлами из этого каталога
+            this.currentDirectory = aDirectory;
+            fill(aDirectory.listFiles());
+        }
+    }
+    //список заполнени€
+    private void fill(File[] files) {
+        //ќчистить список
+        this.directoryEntries.clear();
+        //добавить все файлы в список
+        for (File file : files) {
+            this.directoryEntries.add(file.getAbsolutePath());
+        }
+        //создать адаптер массива, чтобы показать все
+        ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this, R.layout.row, this.directoryEntries);
+        this.setListAdapter(directoryList);
+    }
+    //когда вы нажали на элемент
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        //получить выбранное им€ файла
+
+        int selectionRowID = position;
+        String selectedFileString = this.directoryEntries.get(selectionRowID);
+
+        //если мы выберем Ђ..ї, то вернитесь вверх
+
+        if(selectedFileString.equals("..")){
+            this.upOneLevel();
+        } else {
+            //перейдите к кликному файлу или каталогу, использу€ browseTo ()
+            File clickedFile = null;
+            clickedFile = new File(selectedFileString);
+            if (clickedFile != null)
+                this.browseTo(clickedFile);
+        }
+    }
 }
